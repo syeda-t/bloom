c571c1669c82af4bc868c75d7501e6c5d1ed601f
unknown <naoroz.mahmood@appabx.org>
2019-08-20 11:41:18 -0400

    comment section ADDED!!!!

--- commentsection.js
+++ commentsection.js
@@ -1,11 +1,34 @@
-var comment = [
-  {"name": "Naoroz Mahmood", "date":"10 Apr, 2016", "body": "This is a comment 1"},
-  {"name": "Naoroz Mahmood1", "date":"10 Apr, 2016", "body": "This is a comment 2"},
-  {"name": "Naoroz Mahmood2", "date":"10 Apr, 2016", "body": "This is a comment 3"},
-  {"name": "Naoroz Mahmood3", "date":"10 Apr, 2016", "body": "This is a comment 4"},
-];
-
-for (var i=0; i<comment.length;i++) {
-  var html = '<div class="commentBox"><div class="leftPanelImg"><img src="http://via.placeholder.com/100x100"></div><div class="rightPanel"><span>' +comment[i].name+ '</span><div class="date">'+comment[i].date+'</div><p>'+comment[i].body+'</p></div><div class="clear"></div></div>'
+function render(data) {
+  var html = '<div class="commentBox"><div class="leftPanelImg"><img src="http://via.placeholder.com/75x75"></div><div class="rightPanel"><span>'+data.name+'</span><div class="date">'+data.date+'</div><p>'+data.body+'</p></div><div class="clear"></div></div>';
   $('#container').append(html);
 }
+
+$(document).ready(function() {
+
+  var comment = [];
+
+  if(!localStorage.commentData){
+    localStorage.commentData = [];
+  }else{
+    comment = JSON.parse(localStorage.commentData);
+  };
+
+    for(var i=0; i<comment.length;i++){
+      render(comment[i]);
+    }
+
+  $("#addComment").click(function(){
+    var add0bj = {
+      "name": $("#name").val(),
+      "date": $("#date").val(),
+      "body": $("#bodyText").val(),
+    };
+    comment.push(add0bj);
+    localStorage.commentData = JSON.stringify(comment);
+    render(add0bj);
+    $("#name").val('');
+    $("#date").val('dd/mm/yyyy');
+    $("#bodyText").val('');
+  });
+
+});
